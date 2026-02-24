e38f6a6b8e26164dc916c6109a7472c8e2139fef
omdeeprawat <omdeepr91@gmail.com>
2026-02-24 21:21:32 +0530

    fix: switched to argon2 from bcrypt due to 72 bytes password error

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,10 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 from core.config import settings
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def verify_password(plain_password : str, hashed_password : str) -> bool:
@@ -17,10 +19,11 @@ def get_password_hash(password:str) ->str:
 
 def create_access_token(data : dict, expires_delta: Optional[timedelta] = None) -> str:
   to_encode = data.copy()
-  expire = datetime.utcnow() + (
+  expire = datetime.now(timezone.utc) + (
     expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
   )
   to_encode.update({"exp" : expire})
+
   return jwt.encode(to_encode, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
 
 
