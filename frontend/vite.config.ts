364fc32648d09a13c4968b690bc0f9c658f919b5
omdeeprawat <omdeepr91@gmail.com>
2026-02-24 22:41:41 +0530

    fix: updated vite to also proxy the /auth and route everything through it

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,10 +14,14 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
+      'auth' : {
+        target: 'http://127.0.0.1:800',
+        changeOrigin: true,
+      }
     },
   },
 })
