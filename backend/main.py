0421f4c86e361d3f3db2b0ee96ddb711fe6822d9
omdeeprawat <omdeepr91@gmail.com>
2026-03-03 21:56:43 +0530

    feat(upload) :  added upload import and upload.router

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from core.config import settings
-from routers import health, auth, posts
+from routers import health, auth, posts, upload
 
 import models 
 
@@ -32,7 +32,7 @@ app.add_middleware(
 
 app.include_router(auth.router)
 app.include_router(posts.router)
-
+app.include_router(upload.router)
 
 @app.get("/")
 def root():
